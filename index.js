6d05ee89d9cf6c097e2e5096885af922c22782ba
Elizabeth Catoe <eccatoe2517@gmail.com>
2022-01-12 10:21:37 -0500

    commit tree d33e5332a3c66ab3f3db147007b162472698f415

--- index.js
+++ index.js
@@ -1,28 +1,4 @@
-// Write your solution here!
-const cats=['Milo', 'Otis','Garfield'];
-function destructivelyAppendCat(name){
-    return cats.push('Ralph');
-}
-function destructivelyPrependCat(name){
-    return cats.unshift('Bob');
-}
-function destructivelyRemoveLastCat(){
-    return cats.pop();
-}
-function destructivelyRemoveFirstCat(){
-    return cats.shift();
-}
-function appendCat(name){
-    const copyOfCats=[...cats, 'Broom'];
-    return copyOfCats
-}
-function prependCat(name){
-    const copyOfCats=['Arnold', ...cats];
-    return copyOfCats;
-}
-function removeLastCat(){
-    return [...cats.slice(0,-1)];
-}
-function removeFirstCat(){
-   return [...cats.slice(1)];
-}
+const cats= ["Milo", "Otis", "Garfield"]
+
+let destructivelyAppendCat=cats.push("Ralph")
+
